19642ce548b51e78a953742cdeaae5d61edfe29b
Jose Lima <jalimag0206@gmail.com>
2023-01-29 12:34:53 -0300

    Adicionado função de buscar nota e exportar

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@
 const notesContainer = document.querySelector("#notes-container");
 const noteInput = document.querySelector("#note-content");
 const addNoteBtn = document.querySelector(".add-note");
+const searchInput = document.querySelector("#search-input");
+const exportBtn = document.querySelector("#exports-notes");
+
+
 
 function showNotes(){
     cleanNotes();
@@ -12,10 +16,13 @@ function showNotes(){
     }); 
 }
 
+
 function cleanNotes(){
     notesContainer.replaceChildren([])
 }
 
+
+// Criar uma nova nota
 function addNote(){
     const notes = getNotes();
 
@@ -36,10 +43,16 @@ function addNote(){
     noteInput.value = "";
 }
 
+
+
+// Gerar id aleatorio da nota
 function generateId(){
     return Math.floor(Math.random() * 5000);
 }
 
+
+
+// Criar a nota na tela do usuario
 function createNote(id, content, fixed){
     const element = document.createElement("div")
 
@@ -53,6 +66,8 @@ function createNote(id, content, fixed){
 
     element.appendChild(textarea)
 
+
+
     //Icon de fixar
     const pinIcon = document.createElement("i")
     
@@ -60,12 +75,16 @@ function createNote(id, content, fixed){
 
     element.appendChild(pinIcon);
 
+
+
     //Icon de exlusão
     const deleteIcon = document.createElement("i")
     
     deleteIcon.classList.add(...["bi", "bi-x-lg"]);
 
     element.appendChild(deleteIcon);
+
+
 
     //Icon de duplicar
     const duplicateIcon = document.createElement("i")
@@ -92,6 +111,10 @@ function createNote(id, content, fixed){
     return element;
 }
 
+
+
+
+// Fixar nota
 function toggleFixNote(id){
     const notes = getNotes()
     const targetNote = notes.filter((note) => note.id === id) [0];
@@ -102,12 +125,39 @@ function toggleFixNote(id){
     showNotes();
 }
 
+// Procurar nota
+function searchNotes(search){
+    const searchResults = getNotes().filter((note) => 
+        note.content.includes(search)
+    )
+
+    if(search !== ""){
+        cleanNotes()
+
+        searchResults.forEach((note) => {
+            const noteElement = createNote(note.id, note.content);
+            notesContainer.appendChild(noteElement);
+        });
+
+
+        return;
+    }
+
+    cleanNotes();
+    showNotes();
+}
+
+
+// Deletar nota
 function deleteNote(id, element){
     const notes = getNotes().filter((note) => note.id !== id);
     saveNotes(notes);
     notesContainer.removeChild(element)
 }
 
+
+
+// Duplicar nota
 function copyNote(id){
     const notes = getNotes()
     const targetNote = notes.filter((note) => note.id === id) [0]
@@ -125,6 +175,27 @@ function copyNote(id){
     notes.push(noteObject)
     saveNotes(notes)
 }
+
+
+//Exportar CSV
+function exportData(){
+    const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+
+    const csvString = [
+        ["ID", "Conteúdo", "Fixado?"],
+        ...notes.map((note) => [note.id, note.content, note.fixed]),
+    ]
+    .map((e) => e.join(","))
+    .join("\n");
+
+    const element = document.createElement("a")
+    element.href = "data:text/csv;charset=utf-8" + encodeURI(csvString);
+    element.target = "_blank";
+    element.download = "export.csv";
+    element.click();
+}
+
+
 //Local storage
 function getNotes(){
     const notes = JSON.parse(localStorage.getItem("notes") || "[]");
@@ -134,10 +205,34 @@ function getNotes(){
     return orderedNotes;
 }
 
+
+
+
+// Salvar as notas
 function saveNotes(notes){
     localStorage.setItem("notes", JSON.stringify(notes));
 }
 
+//EVENTOS
+
+//Adiciona nota ao clicar
 addNoteBtn.addEventListener("click", () => addNote());
+
+//Adiciona a nota com Enter
+noteInput.addEventListener("keydown", (e) => {
+    if(e.key === "Enter"){
+        addNote();
+    }
+});
+
+//Procurar nota pelo input
+searchInput.addEventListener("keyup", (e) => {
+    const search = e.target.value;
+    searchNotes(search);
+});
+
+exportBtn.addEventListener("click", () => {
+    exportData();
+});
 
 showNotes()
